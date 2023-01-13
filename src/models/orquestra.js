4a577e3050655f28267ee6dfd78e629e8844b254
Junior Santos <juniorsantos@tjro.jus.br>
2023-01-13 10:21:14 -0400

    Migrando o projeto

---
+++ src/models/orquestra.js
@@ -0,0 +1,12 @@
+export default class Orquestra {
+  constructor(id, name, acronyms, address, email, phone, photo, dt_end) {
+    this.id = id
+    this.name = name
+    this.acronyms = acronyms
+    this.address = address
+    this.email = email
+    this.phone = phone
+    this.photo = photo
+    this.dt_end = dt_end
+  }
+}
